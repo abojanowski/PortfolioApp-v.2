c91801ad3ee8ddc05c00864846577f6a693ea1a4
Arek Bojanowski <abojanowski>
2015-06-16 16:36:06 +0200

    I changed migrations

--- db/migrate/20150616104339_create_works.rb
+++ db/migrate/20150616104339_create_works.rb
@@ -2,7 +2,7 @@ class CreateWorks < ActiveRecord::Migration
   def change
     create_table :works do |t|
       t.string :picture
-      t.references :album_id, index: true, foreign_key: true
+      t.references :album, index: true, foreign_key: true
 
       t.timestamps null: false
     end
--- db/migrate/20150616110114_remove_album_id_id_from_works.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAlbumIdIdFromWorks < ActiveRecord::Migration
-  def change
-  	remove_column :works, :album_id_id, :integer
-  end
-end
